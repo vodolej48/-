b23abec8481b6bf481b8a28e03dd39b560fb1fd7
vodolej48 <sergej.trushin2010@yandex.ru>
2024-09-20 10:47:12 +0300

    20.09.

---
+++ pythonProject1/main.py
@@ -0,0 +1,7 @@
+first_name = 'количество выполненных ДЗ'
+homework_count = 12
+second_name = 'количество затраченных часов'
+number_of_hours_spent_count = 1.5
+third_name = 'Python'
+time_for_one_task = number_of_hours_spent_count / homework_count
+print('Курс:',third_name,', всего задач:', homework_count,', затрачено часов:', number_of_hours_spent_count, ', среднее время выполнения', time_for_one_task,'часа' )
