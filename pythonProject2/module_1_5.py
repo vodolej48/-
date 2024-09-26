5d79b85bf95ebc9a6056cc767e84c731d987a28a
vodolej48 <sergej.trushin2010@yandex.ru>
2024-09-26 19:27:48 +0300

    26.09.

---
+++ pythonProject2/module_1_5.py
@@ -0,0 +1,9 @@
+immutable_var = tuple([1,3,5,'string', False,[1,3,5]])
+print(type(immutable_var))
+print(immutable_var)
+mutable_list = [1,3,5,'string', False,[1,3,5]]
+print(mutable_list)
+print(type(mutable_list))
+mutable_list[2] = 8
+print(mutable_list)
+immutable_var[2] = 8
