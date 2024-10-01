0c539a0dedd2ea6f0dfe36701f62f06adbbf3857
vodolej48 <sergej.trushin2010@yandex.ru>
2024-10-01 15:28:43 +0300

    01.10.

---
+++ pythonProject2/module-1_6.py
@@ -0,0 +1,16 @@
+my_dict = {'serg':35,'alina':46,'denis':57}
+print(my_dict)
+print(my_dict ['alina'])
+my_dict ['taya'] = 68
+print(my_dict)
+my_dict.update ({'anton':79,'sacha':90})
+print(my_dict)
+a =  my_dict.pop('alina')
+print(my_dict)
+print(a)
+my_set = {4,6,8,4,7,6,False,'apple'}
+print(my_set)
+my_set.update([5,'cocount',True,8,(18,19,20)])
+print(my_set)
+my_set.remove('apple')
+print(my_set)
