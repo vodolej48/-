adc9a5d138a122327c8183f7c168b79650d2c013
vodolej48 <sergej.trushin2010@yandex.ru>
2024-10-11 10:51:22 +0300

    11.10.

--- pythonProject2/module_1.py
+++ pythonProject2/module_1.py
@@ -1,23 +1,3 @@
-#students = ['rush','kil','varsha']
-#estimation = [1,4,5]
-#print('original key list is:'+ str(students))
-#print('original value list is:'+ str(estimation))
-#for key in students:
-    #for value in  estimation:
-      # res [key] = value
-       #test_values.remove(value)
-      # break
-        #print('resultant distionary is:' + str(res))
-#estimation =['rush','kil','varsha']
-#students = [1,4,5]
-#print('ключ:'+ str(estimation))
-#print('значение:'+ str(students))
-#res = {estimation[i]: students[i] for i in range(len(estimation))}
-#print('журнал:' + str(res))
-#students = ['rush','kil','varsha']
-#estimation = [1,4,5]
-#my_dict = dict(zip(keys,values ))
-#print(my_dict)
 grades = [[5,3,3,5,4],[2,2,2,3],[4,5,5,2],[4,4,3],[5,5,5,4,5]]
 students = {'Johnny','Bilbo','Steve','Khendrik','Aaron'}
 students = list(students)
