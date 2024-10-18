595b5b46e74ec85259e55b4e9d7372048e8b7254
vodolej48 <sergej.trushin2010@yandex.ru>
2024-10-18 19:28:27 +0300

    18.10.

---
+++ pythonProject2/module_2_4.py
@@ -0,0 +1,31 @@
+# phone_book = { 'serg' : 89202410238, 'taya': 89205094341 }
+# phone_book['serg'] = 13
+# phone_book['anton'] = 8596
+# phone_book.update({'sasha': 678,'alex': 890})
+# print(phone_book)
+# a = phone_book.pop('taya')
+# print(phone_book)
+# print(a)
+# list_ = [1,2,3]
+# list_.pop(0)
+# print(list_)
+# print(phone_book.keys())
+# print(phone_book.values())
+# print(phone_book.items())
+names = [1, 2, 3, 5, 6, -3, 4, -2]
+index = 0
+while index < len(names):
+    if names[index] < 0:
+        break
+    elif names[index] > 0:
+        print(names)
+        index +=1
+
+
+
+
+
+
+
+
+
