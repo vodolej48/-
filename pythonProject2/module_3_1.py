638f824dc95399a735d84793a026ebd876f345a1
vodolej48 <sergej.trushin2010@yandex.ru>
2025-01-26 10:14:54 +0300

    26.01.25

---
+++ pythonProject2/module_3_1.py
@@ -0,0 +1,19 @@
+calls = 0
+
+def count_calls():
+    global calls
+    calls += 1
+
+def string_info(string):
+    count_calls()
+    return (len(string), string.upper(), string.lower())
+
+def is_contains(string, strings):
+    count_calls()
+    return string.lower() in map(str.lower,strings)
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))
+print(is_contains('cycle', ['recycling', 'cyclic']))
+print(calls)
